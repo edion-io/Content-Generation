6fc88cb75c263ba819e60f1f395e5a09f0b5eebb
TheGrandNobody <thegrandnobody@protonmail.com>
2024-08-04 16:03:57 +0200

    Example file + new questoins

--- misc.py
+++ misc.py
@@ -1,7 +1,10 @@
 import fitz
 from copy import deepcopy
 
-FILE = 'books/German/The Everything Essential German Book All you need to Learn German in no time (Edward Swick) (Z-Library).pdf'
+FILE = 'books/German/German Language Learning This Book includes Learn German for Beginners, Phrase Book, Short Stories. Perfect For Travel Get... (Language Building Lab) (Z-Library).pdf'
+
+def is_bold(font_name):
+    return 'Bold' in font_name
 
 def check_num(answer):
     i = 1
@@ -33,74 +36,43 @@ def sort_key(s):
 if __name__ == "__main__":
     # Open the PDF file
     pdf_document = fitz.open(FILE)
-
-    unit = 2
-    subunit = 0
-    collecting = 0
     texts = {}
-    answer = None
-
+    exs = []
+    collecting = False
     # Get all answers
-    for page_num in range(314, len(pdf_document)):
+    for page_num in range(40, 211):
         # Load the page
         page = pdf_document.load_page(page_num)
-
-        # Iterate through the text's
-        text = page.get_text("text")
-        for line in text.split('\n'):
-            if "Chapter" in line:
-                continue
-            if f'{unit}-{subunit + 1}' == line.replace("Exercise", "").strip() or f'{unit + 1}-1' == line.replace("Exercise", "").strip() or collecting == 10:
-                if answer is not None:
-                    texts[f'{unit}-{subunit}'] = answer
-                if line.replace("Exercise", "").strip() == f'{unit + 1}-1':
-                    subunit = 1
-                    unit += 1
-                else:
-                    subunit += 1
-                collecting = 1
-            elif collecting == 1:
-                answer = line + '\n'
-                collecting += 1
-            elif collecting == 2:
-                answer += line + '\n'
-                if subunit == 2 and unit == 19 and "said Father cannot work" in line:
-                    collecting = 10
-
-    keys = sorted(deepcopy(list(texts.keys())), key=sort_key)
-    unit, subunit, done = get_unit_subunit(keys)
-    max_num = check_num(texts[f'{unit}-{subunit}'])
-    question = None
-    collecting = 0
-    final = False
-
-    for page_num in range(33, 313):
-        # Load the page
-        page = pdf_document.load_page(page_num)
-        text = page.get_text("text")
-        for i, line in enumerate(text.split('\n')):
-            if f"{unit}-{subunit}" in line.strip() or (final and f'{max_num}.' in line):
-                if (final and f'{max_num}.' in line):
-                    question += line + '\n'
-                if question is not None:
-                    texts[f'{p_unit}-{p_subunit}'] = f'German T D G (With Answer)\n{question}\nAnswers\n{texts[f'{p_unit}-{p_subunit}']}'
-                p_unit, p_subunit = unit, subunit
-                if final:
-                    break
-                if done:
-                    final = True
-                else:
-                    unit, subunit, done = get_unit_subunit(keys)
-                collecting = 1
-                max_num = check_num(texts[f'{p_unit}-{p_subunit}'])
-            elif collecting == 1:
-                question = line + '\n'
-                collecting = 2
-            elif collecting == 2:
-                question += line + '\n'
-                if f'{max_num}.' in line:
-                    collecting = 0
-    print(texts)
+        blocks = page.get_text('dict')['blocks']
+        for block in blocks:
+            if 'lines' in block:
+                for line in block['lines']:
+                    for span in line['spans']:
+                        text = span['text']
+                        font_name = span['font']
+                        bold = is_bold(font_name)
+                        if page_num < 43:
+                            print(exs, text)
+                        if text.strip() == "Exercises" and bold:
+                            exs.clear()
+                            collecting = 1
+                            continue
+                        elif text.strip() == "Answers" and bold:
+                            for x in exs:
+                                texts[x] += '\nAnswers\n'
+                            collecting = 2
+                        elif collecting:
+                            if bold:
+                                if collecting in [1,2]:
+                                    exercise = text
+                                if 'Chapter' in text:
+                                    collecting = 0
+                                elif collecting == 1:
+                                    exs.append(exercise)
+                                if exercise not in texts:
+                                    texts[exercise] = 'German T D G (With Answer)\n'
+                            else:
+                                texts[exercise] += text + '\n'
     with open("german.txt", 'w') as f:
         for val in texts.values():
             f.write(f'{val}\n\n')
