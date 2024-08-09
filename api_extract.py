6926d108d843112e4a3e2e9335108dcc251e57cf
TheGrandNobody <thegrandnobody@protonmail.com>
2024-08-09 14:04:53 +0200

    Add Cambridge math questions

--- api_extract.py
+++ api_extract.py
@@ -14,7 +14,7 @@ CLOUDINARY_API = "667797151493891"
 CLOUDINARY_SECRET = "WuKdiXBzcwzUgOsdOey5J9E8k7c"
 
 # Specify the prompt for the chat completion task
-prompt = "1. Extract all exercises from the pages .\n2. Output them in this format:\n*NEW*\n[Type or Topic of Activity/Exercises]\n[Replace with given exercise(s) of given page, separated by newline ]\n\n 3. Output exercises per page as if it was one large activity (one single 'NEW' and one label). 4. If an activity or exercise needs or refers to one or more images or tables, add a description of the image(s) to the above template in the form:\n[STRDGRM] [Detailed description of the image that a blind person can use to visualize what is needed, without even seeing the image] [STPDGRM]\n\n5. Only output what is asked of you."
+prompt = "1. Extract all exercises from the pages .\n2. Output them in this format:\n*NEW*\n[Max 5 words describing the type of exercise]\n[Replace with extracted text]\n[Situational: Diagram description]\n\n 3. Some exercises continue over to another page, make sure you get that too (should not be separate). 4. If an activity or exercise needs or refers to one or more images or tables, add a description of the image(s) to the above template in the form:\n[STRDGRM] [Detailed description of the image that a blind person can use to visualize what is needed, without even seeing the image] [STPDGRM]\n\n5. Only output what is asked of you."
 
 
 if __name__ == "__main__":
